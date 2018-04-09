79a103402a4c0ce017faadeb43fff6fcd7966101
hantao <hanntao@163.com>
2018-04-09 23:55:20 +0800

    init

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+// 设置为常量
+define( "DB_IP", "127.0.0.1" );
+define( "DB_USER", "root" );
+define( "DB_pwd", "" );
+define( "DB_NAME", "baixiu" );
+
+
+?>
---
+++ fn.php
@@ -0,0 +1,22 @@
+<?php
+// 引入config.php文件
+require_once "./config.php";
+
+// 封装函数
+// 需要一个读取单个数据的函数
+function select_single( $sql ) {
+  // 连接数据库
+  $conn = mysqli_connect( DB_IP, DB_USER, DB_PWD, DB_NAME );
+  // 查询语句拿出来的是reader对象，最后一定要free
+  $reader = mysqli_query( $conn, $sql );
+  // fetch用row来拿，因为它本身只有一个数据，没有必要记住它键的名字
+  mysqli_fetch_row( $reader ); //得到的是一个长度为1的数组
+  // 释放mysqli
+  mysqli_free_result( $reader );
+  // 
+  mysqli_close( $conn );
+  // 返回数据
+  return $item[0];
+}
+
+?>
